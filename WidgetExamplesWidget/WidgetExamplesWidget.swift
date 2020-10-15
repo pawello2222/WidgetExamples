00b2473bf1b68c397eb197180d4332612e7664d5
pawello2222 <pawello2222@gmail.com>
2020-10-15 15:02:02 +0200

    Added Clock Widget

--- WidgetExamplesWidget/WidgetExamplesWidget.swift
+++ WidgetExamplesWidget/WidgetExamplesWidget.swift
@@ -6,63 +6,12 @@
 //  Copyright © 2020 Pawel Wiszenko. All rights reserved.
 //
 
-import WidgetKit
 import SwiftUI
-
-struct Provider: TimelineProvider {
-    func placeholder(in context: Context) -> SimpleEntry {
-        SimpleEntry(date: Date())
-    }
-
-    func getSnapshot(in context: Context, completion: @escaping (SimpleEntry) -> ()) {
-        let entry = SimpleEntry(date: Date())
-        completion(entry)
-    }
-
-    func getTimeline(in context: Context, completion: @escaping (Timeline<Entry>) -> ()) {
-        var entries: [SimpleEntry] = []
-
-        // Generate a timeline consisting of five entries an hour apart, starting from the current date.
-        let currentDate = Date()
-        for hourOffset in 0 ..< 5 {
-            let entryDate = Calendar.current.date(byAdding: .hour, value: hourOffset, to: currentDate)!
-            let entry = SimpleEntry(date: entryDate)
-            entries.append(entry)
-        }
-
-        let timeline = Timeline(entries: entries, policy: .atEnd)
-        completion(timeline)
-    }
-}
-
-struct SimpleEntry: TimelineEntry {
-    let date: Date
-}
-
-struct WidgetExamplesWidgetEntryView : View {
-    var entry: Provider.Entry
-
-    var body: some View {
-        Text(entry.date, style: .time)
-    }
-}
+import WidgetKit
 
 @main
-struct WidgetExamplesWidget: Widget {
-    let kind: String = "WidgetExamplesWidget"
-
-    var body: some WidgetConfiguration {
-        StaticConfiguration(kind: kind, provider: Provider()) { entry in
-            WidgetExamplesWidgetEntryView(entry: entry)
-        }
-        .configurationDisplayName("My Widget")
-        .description("This is an example widget.")
-    }
-}
-
-struct WidgetExamplesWidget_Previews: PreviewProvider {
-    static var previews: some View {
-        WidgetExamplesWidgetEntryView(entry: SimpleEntry(date: Date()))
-            .previewContext(WidgetPreviewContext(family: .systemSmall))
+struct WidgetExamplesWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        ClockWidget()
     }
 }
