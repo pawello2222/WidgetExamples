b355d618c26be9935c2a2b4af24016218be202f5
pawello2222 <pawello2222@gmail.com>
2020-10-15 16:14:10 +0200

    Added Network Widget

---
+++ WidgetExamplesWidget/NetworkWidget/CurrencyWebRepository.swift
@@ -0,0 +1,76 @@
+//
+//  CurrencyWebRepository.swift
+//  WidgetExamplesWidgetExtension
+//
+//  Created by Pawel on 15/10/2020.
+//  Copyright © 2020 Pawel Wiszenko. All rights reserved.
+//
+
+import Combine
+import Foundation
+
+class CurrencyWebRepository: WebRepository {
+    let session: URLSession
+
+    init(session: URLSession = .shared) {
+        self.session = session
+    }
+}
+
+extension CurrencyWebRepository {
+    struct ErrorResponse: Decodable {
+        let error: String
+    }
+}
+
+extension CurrencyWebRepository {
+    func fetchAPIResource<Resource>(_ resource: Resource) -> AnyPublisher<Resource.Response, APIError> where Resource: APIResource {
+        guard let url = resource.url else {
+            let error = APIError.invalidRequest(description: "Invalid `resource.url`: \(String(describing: resource.url))")
+            return Fail(error: error).eraseToAnyPublisher()
+        }
+        return fetch(url: url)
+            .flatMap(decode)
+            .eraseToAnyPublisher()
+    }
+
+    func decode<Response>(data: Data) -> AnyPublisher<Response, APIError> where Response: Decodable {
+        if let response = try? JSONDecoder().decode(Response.self, from: data) {
+            return Just(response).setFailureType(to: APIError.self).eraseToAnyPublisher()
+        }
+        do {
+            let errorResponse = try JSONDecoder().decode(ErrorResponse.self, from: data)
+            return Fail(error: .invalidRequest(description: errorResponse.error)).eraseToAnyPublisher()
+        } catch {
+            return Fail(error: .unknown).eraseToAnyPublisher()
+        }
+    }
+}
+
+struct CurrencyRatesResource: APIResource {
+    let serverPath = "api.exchangeratesapi.io"
+    let methodPath: String
+    var queryItems: [URLQueryItem]?
+
+    init(_ base: String, date: Date) {
+        methodPath = "/" + Self.dateFormatter.string(from: date)
+        queryItems = [URLQueryItem(name: "base", value: base)]
+    }
+}
+
+extension CurrencyRatesResource {
+    static let dateFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.locale = Locale(identifier: "en_US_POSIX")
+        formatter.dateFormat = "yyyy-MM-dd"
+        return formatter
+    }()
+}
+
+extension CurrencyRatesResource {
+    struct Response: Decodable {
+        let base: String
+        let date: String
+        let rates: [String: Double]
+    }
+}
---
+++ WidgetExamplesWidget/NetworkWidget/WebRepository.swift
@@ -0,0 +1,53 @@
+//
+//  WebRepository.swift
+//  WidgetExamplesWidgetExtension
+//
+//  Created by Pawel on 15/10/2020.
+//  Copyright © 2020 Pawel Wiszenko. All rights reserved.
+//
+
+import Combine
+import Foundation
+
+protocol WebRepository {
+    var session: URLSession { get }
+}
+
+extension WebRepository {
+    func fetch(url: URL) -> AnyPublisher<Data, APIError> {
+        session.dataTaskPublisher(for: URLRequest(url: url))
+            .mapError { error in
+                if error.code.rawValue == -1009 {
+                    return .offline
+                }
+                return .network(code: error.code.rawValue, description: error.localizedDescription)
+            }
+            .map(\.data)
+            .eraseToAnyPublisher()
+    }
+}
+
+protocol APIResource {
+    associatedtype Response: Decodable
+    var serverPath: String { get }
+    var methodPath: String { get }
+    var queryItems: [URLQueryItem]? { get }
+}
+
+extension APIResource {
+    var url: URL? {
+        var components = URLComponents()
+        components.scheme = "https"
+        components.host = serverPath
+        components.path = methodPath
+        components.queryItems = queryItems
+        return components.url
+    }
+}
+
+enum APIError: Error {
+    case offline
+    case network(code: Int, description: String)
+    case invalidRequest(description: String)
+    case unknown
+}
