548ece9d8dd90f72245bb44830d614b7fe3ba4b3
pawello2222 <pawello2222@gmail.com>
2020-10-15 15:13:01 +0200

    Added Countdown Widget

---
+++ WidgetExamplesWidget/CountdownWidget/CountdownWidget.swift
@@ -0,0 +1,62 @@
+//
+//  CountdownWidget.swift
+//  WidgetExamples
+//
+//  Created by Pawel on 15/10/2020.
+//  Copyright © 2020 Pawel Wiszenko. All rights reserved.
+//
+
+import SwiftUI
+import WidgetKit
+
+private struct Provider: TimelineProvider {
+    func placeholder(in context: Context) -> SimpleEntry {
+        SimpleEntry(date: Date(), displayDate: Date())
+    }
+
+    func getSnapshot(in context: Context, completion: @escaping (SimpleEntry) -> Void) {
+        let entry = SimpleEntry(date: Date(), displayDate: Date())
+        completion(entry)
+    }
+
+    func getTimeline(in context: Context, completion: @escaping (Timeline<SimpleEntry>) -> Void) {
+        let currentDate = Date()
+        let firstDate = Calendar.current.date(byAdding: .second, value: 50, to: currentDate)!
+        let secondDate = Calendar.current.date(byAdding: .second, value: 60, to: currentDate)!
+
+        let entries = [
+            SimpleEntry(date: currentDate, displayDate: secondDate),
+            SimpleEntry(date: firstDate, displayDate: secondDate, isDateClose: true),
+        ]
+
+        let timeline = Timeline(entries: entries, policy: .atEnd)
+        completion(timeline)
+    }
+}
+
+private struct SimpleEntry: TimelineEntry {
+    let date: Date
+    let displayDate: Date
+    var isDateClose = false
+}
+
+private struct CountdownWidgetEntryView: View {
+    var entry: Provider.Entry
+
+    var body: some View {
+        Text(entry.displayDate, style: .timer)
+            .foregroundColor(entry.isDateClose ? .red : .primary)
+    }
+}
+
+struct CountdownWidget: Widget {
+    let kind: String = "CountdownWidget"
+
+    var body: some WidgetConfiguration {
+        StaticConfiguration(kind: kind, provider: Provider()) { entry in
+            CountdownWidgetEntryView(entry: entry)
+        }
+        .configurationDisplayName("Countdown Widget")
+        .description("This is an example widget.")
+    }
+}
