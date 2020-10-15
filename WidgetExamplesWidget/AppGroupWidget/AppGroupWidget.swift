07c42c372067745cae527c1c90d9d1e1ccb022de
pawello2222 <pawello2222@gmail.com>
2020-10-15 16:23:10 +0200

    Added AppGroup Widget

---
+++ WidgetExamplesWidget/AppGroupWidget/AppGroupWidget.swift
@@ -0,0 +1,53 @@
+//
+//  AppGroupWidget.swift
+//  WidgetExamplesWidgetExtension
+//
+//  Created by Pawel on 15/10/2020.
+//  Copyright © 2020 Pawel Wiszenko. All rights reserved.
+//
+
+import SwiftUI
+import WidgetKit
+
+private struct Provider: TimelineProvider {
+    func placeholder(in context: Context) -> SimpleEntry {
+        SimpleEntry(date: Date())
+    }
+
+    func getSnapshot(in context: Context, completion: @escaping (SimpleEntry) -> Void) {
+        let entry = SimpleEntry(date: Date())
+        completion(entry)
+    }
+
+    func getTimeline(in context: Context, completion: @escaping (Timeline<Entry>) -> Void) {
+        let midnight = Calendar.current.startOfDay(for: Date())
+        let nextMidnight = Calendar.current.date(byAdding: .day, value: 1, to: midnight)!
+        let entries = [SimpleEntry(date: midnight)]
+        let timeline = Timeline(entries: entries, policy: .after(nextMidnight))
+        completion(timeline)
+    }
+}
+
+private struct SimpleEntry: TimelineEntry {
+    let date: Date
+}
+
+private struct AppGroupWidgetEntryView: View {
+    var entry: Provider.Entry
+
+    var body: some View {
+        Text(entry.date, style: .timer)
+    }
+}
+
+struct AppGroupWidget: Widget {
+    let kind: String = "AppGroupWidget"
+
+    var body: some WidgetConfiguration {
+        StaticConfiguration(kind: kind, provider: Provider()) { entry in
+            AppGroupWidgetEntryView(entry: entry)
+        }
+        .configurationDisplayName("AppGroup Widget")
+        .description("This is an example widget.")
+    }
+}
