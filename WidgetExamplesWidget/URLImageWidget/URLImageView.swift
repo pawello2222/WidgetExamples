779fe39751583fbf169b9cf31449ac167783e585
pawello2222 <pawello2222@gmail.com>
2020-10-15 17:39:22 +0200

    Added URLImage Widget

---
+++ WidgetExamplesWidget/URLImageWidget/URLImageView.swift
@@ -0,0 +1,23 @@
+//
+//  URLImageView.swift
+//  WidgetExamplesWidgetExtension
+//
+//  Created by Pawel on 15/10/2020.
+//  Copyright © 2020 Pawel Wiszenko. All rights reserved.
+//
+
+import SwiftUI
+
+struct URLImageView: View {
+    let url: URL
+
+    @ViewBuilder
+    var body: some View {
+        if let data = try? Data(contentsOf: url), let uiImage = UIImage(data: data) {
+            Image(uiImage: uiImage)
+                .resizable()
+        } else {
+            Image(systemName: "photo")
+        }
+    }
+}
---
+++ WidgetExamplesWidget/URLImageWidget/URLImageWidget.swift
@@ -0,0 +1,54 @@
+//
+//  URLImageWidget.swift
+//  WidgetExamplesWidgetExtension
+//
+//  Created by Pawel on 15/10/2020.
+//  Copyright © 2020 Pawel Wiszenko. All rights reserved.
+//
+
+import SwiftUI
+import WidgetKit
+
+private struct Provider: TimelineProvider {
+    func placeholder(in context: Context) -> SimpleEntry {
+        SimpleEntry(date: Date())
+    }
+
+    func getSnapshot(in context: Context, completion: @escaping (SimpleEntry) -> Void) {
+        let entry = SimpleEntry(date: Date())
+        completion(entry)
+    }
+
+    func getTimeline(in context: Context, completion: @escaping (Timeline<Entry>) -> Void) {
+        let midnight = Calendar.current.startOfDay(for: Date())
+        let nextMidnight = Calendar.current.date(byAdding: .day, value: 1, to: midnight)!
+        let entries = [SimpleEntry(date: midnight)]
+        let timeline = Timeline(entries: entries, policy: .after(nextMidnight))
+        completion(timeline)
+    }
+}
+
+private struct SimpleEntry: TimelineEntry {
+    let date: Date
+}
+
+private struct URLImageWidgetEntryView: View {
+    var entry: Provider.Entry
+
+    var body: some View {
+        URLImageView(url: URL(string: "https://raw.githubusercontent.com/hjnilsson/country-flags/master/png1000px/pl.png")!)
+            .aspectRatio(contentMode: .fill)
+    }
+}
+
+struct URLImageWidget: Widget {
+    let kind: String = "URLImageWidget"
+
+    var body: some WidgetConfiguration {
+        StaticConfiguration(kind: kind, provider: Provider()) { entry in
+            URLImageWidgetEntryView(entry: entry)
+        }
+        .configurationDisplayName("URLImage Widget")
+        .description("This is an example widget.")
+    }
+}
